fd51053d7aa503cfe0d5f7f4b4a978d089afe5c7
Mark Joseph Alconcel <109438699+MJAlconcel8@users.noreply.github.com>
2024-11-02 17:15:22 -0400

    added more boiler plate, the bot works

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import discord
 class Client(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
-        
+    
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if message.content.startswith('hello'):
+            await message.channel.send(f'hi there {message.author}')
         
 intents = discord.Intents.default()
 intents.message_content = True
